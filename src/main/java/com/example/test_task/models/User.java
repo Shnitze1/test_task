de73c98e192ae5aa68eb08d1647eb1c48cae5b91
Alexander <abom28908@gail.com>
2025-05-29 09:46:35 +0500

    Created a project. Implemented a basic user model and created a repository.

---
+++ src/main/java/com/example/test_task/models/User.java
@@ -0,0 +1,25 @@
+package com.example.test_task.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+}
---
+++ src/main/java/com/example/test_task/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.test_task.repositories;
+
+import com.example.test_task.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
